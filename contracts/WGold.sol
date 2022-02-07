8e048b114c78d74d79cd03174e0d780a233f5e98
jimmy20222022 <jimmy20222022@gmail.com>
2022-02-07 15:44:14 +0800

    Remove approve function

--- contracts/WGold.sol
+++ contracts/WGold.sol
@@ -15,12 +15,6 @@ contract WGOLD is ERC20 {
         _;
     }
 
-    function approve(address delegate, uint256 numTokens) public override returns (bool) {
-        allowed[msg.sender][delegate] = numTokens;
-        emit Approval(msg.sender, delegate, numTokens);
-        return true;
-    }
-
     // Allow admin to mint the token
     function mint(address account, uint amount) public onlyAdmin {
         _mint(account, amount);
